00d872cea79ece801fc6bf97a8d7c10c2d0284b4
meshjohn <meshjohn1@gmail.com>
2025-10-03 16:54:01 +0300

    implement daily news

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { inngest } from "@/lib/inngest/client";
-import { sendingSingUpEmail } from "@/lib/inngest/function";
+import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/function";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [sendingSingUpEmail],
+  functions: [sendSignUpEmail, sendDailyNewsSummary],
 });
